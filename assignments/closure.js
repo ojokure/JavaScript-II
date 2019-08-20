e07c5e7e3036058de09d609bb1a5d62cda4f8154
ojokure <ojokuredim@gmail.com>
2019-08-20 20:00:10 +0100

    first closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,6 +3,15 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+function person(){
+  const first_name = "ojo";
+  const last_name = "drinkwater";
+
+  return function( ) {
+    const myName = first_name + " " + last_name
+    return myName;
+  }
+}
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
